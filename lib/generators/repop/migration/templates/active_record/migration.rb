0b6047bb6a9fb4e40712b3a6f402d14bb45f1b32
MdreW <andrea.ranaldi@gmail.com>
2012-11-24 08:26:14 +0100

    Cleaned migration

--- lib/generators/repop/migration/templates/active_record/migration.rb
+++ lib/generators/repop/migration/templates/active_record/migration.rb
@@ -2,14 +2,14 @@ class RepopMigration < ActiveRecord::Migration
   def self.up
     create_table :repops do |t|
       t.references :repopsable, :polymorphic => true
-      t.string :context
       t.string :key
       t.string :value
 
       t.timestamps
     end
 
-    add_index :options, [:repop_id, :context]
+    add_index :options, :repopable_id
+    add_index :options, :repopable_type
   end
 
   def self.down
